60b9d91aa20266e217867b4ef778fa0e00fcfce3
ChengHoHin-2425 <240032089@stu.vtc.edu.hk>
2025-04-24 10:28:37 +0000

    Finally update

---
+++ migrations/versions/4723e595508d_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 4723e595508d
+Revises: 81f5c45127b6
+Create Date: 2025-04-21 15:26:59.000364
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '4723e595508d'
+down_revision = '81f5c45127b6'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
