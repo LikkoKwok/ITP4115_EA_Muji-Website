b558113f73ba5ac0b90d6489aadfcc395a7ffa3c
Likko <240377264@stu.vtc.edu.hk>
2025-06-26 12:16:00 +0000

    testing

--- app/models.py
+++ app/models.py
@@ -17,6 +17,7 @@ followers = db.Table(
     'followers',
     db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
     db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+    
 )
 
 
---
+++ migrations/versions/048fb0853028_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 048fb0853028
+Revises: f3aee1f32e3d, 761558fed647
+Create Date: 2025-06-16 16:21:18.108328
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '048fb0853028'
+down_revision = ('f3aee1f32e3d', '761558fed647')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
